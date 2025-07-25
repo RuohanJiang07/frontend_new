bbef69acced45c0f9cad5dd7db11bde36ae08df3
Yilin Zhao <yz4742@columbia.edu>
2025-07-26 06:54:13 +0800

    Revert "experimental: home page"
    This reverts commit 39b2c1195b45d9082fb9f927e4ac13cf8b14f8e8.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/mainPages/login/LoginPage'
 import MyWorkspaces from './pages/mainPages/myWorkspaces/myWorkspaces'
 import BookWorkspaces from './pages/workspacePages/workspaceFrame/BookWorkspaces'
-import HomePage from './pages/mainPages/home/HomePage'
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<MyWorkspaces />} />
-        <Route path="/home" element={<HomePage />} />
         <Route path="/workspaces" element={<MyWorkspaces />} />
         <Route path="/workspace" element={<BookWorkspaces />} />
       </Routes>
